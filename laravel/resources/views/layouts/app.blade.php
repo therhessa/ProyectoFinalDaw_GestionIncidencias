aabe0d2ad38a4b746449a80452bf468099c71cd8
therhessa <therhessa@gmail.com>
2022-01-13 18:46:51 +0100

    main

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -83,6 +83,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3">
+                        @include('includes.menu')
 
                     </div>
                     <div class="col-md-9">
