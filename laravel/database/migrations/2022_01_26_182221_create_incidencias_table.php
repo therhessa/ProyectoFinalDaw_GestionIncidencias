d5018f910478dd8053330ac36b563ef0ba26b878
therhessa <therhessa@gmail.com>
2022-01-26 19:24:19 +0100

    main

---
+++ laravel/database/migrations/2022_01_26_182221_create_incidencias_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIncidenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidencias', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('severity', 1);
+            $table->bigInteger('categoria_id')->unsigned();
+            $table->foreign('categoria_id')->references('id')->on('categorias');
+            $table->bigInteger('soporte_id')->unsigned()->nullable();
+            $table->foreign('soporte_id')->references('id')->on('soportes');
+            $table->bigInteger('cliente_id')->unsigned();
+            $table->foreign('cliente_id')->references('id')->on('users');
+            $table->bigInteger('tecnico_id')->unsigned();
+            $table->foreign('tecnico_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidencias');
+    }
+}
